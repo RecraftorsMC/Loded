f2bb3408ea875613715bfe3be9b2bba7a1628baf
Nerjal Nosk <nosk.nerjal@gmx.com>
2023-12-27 23:43:08 +0100

    1.20

--- common/src/main/java/mc/recraftors/loded/Loded.java
+++ common/src/main/java/mc/recraftors/loded/Loded.java
@@ -1,11 +1,11 @@
 package mc.recraftors.loded;
 
 import net.minecraft.item.Item;
-import net.minecraft.tag.TagKey;
+import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
+import net.minecraft.registry.RegistryKeys;
 
 public class Loded {
 	public static final String MOD_ID = "loded";
-	public static final TagKey<Item> COMPASS_TAG = TagKey.of(Registry.ITEM_KEY, new Identifier(MOD_ID, "compasses"));
+	public static final TagKey<Item> COMPASS_TAG = TagKey.of(RegistryKeys.ITEM, new Identifier(MOD_ID, "compasses"));
 }
